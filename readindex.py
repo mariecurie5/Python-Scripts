a6bfe501eb565c3cb84d19bc35caaf143dc1443b
marie <mariecurieramirez@gmail.com>
2022-08-10 22:26:10 -0700

    initial working verson of readindex.py

---
+++ readindex.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import argparse
+import sys
+import re
+import os.path
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--file', type=str, required=True)
+    parser.add_argument('--index', type=str, default="autoread")
+
+    args = parser.parse_args()
+
+    if (os.path.exists(args.file)) is False:
+        print(f"ERROR: File ({args.file}) does not exist!")
+        exit()
+
+
+
